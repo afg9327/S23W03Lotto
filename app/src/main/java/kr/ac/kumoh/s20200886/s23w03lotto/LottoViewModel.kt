96596494565d20d133f3131ff1605faca1564466
afg9327 <93969640+afg9327@users.noreply.github.com>
2023-10-05 14:10:59 +0900

    💩옵저버 추가

---
+++ app/src/main/java/kr/ac/kumoh/s20200886/s23w03lotto/LottoViewModel.kt
@@ -0,0 +1,32 @@
+package kr.ac.kumoh.s20200886.s23w03lotto
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlin.random.Random
+
+class LottoViewModel : ViewModel( ) {
+    //private var _numbers = intArrayOf(0, 0, 0, 0, 0, 0)
+    private var _numbers = MutableLiveData<IntArray>(IntArray(6) { 0 })
+    val numbers: LiveData<IntArray>
+        get() = _numbers
+    fun generate() {
+        var num = 0
+        var newNumbers = IntArray(6){0 }
+        for (i in newNumbers.indices) {
+//            _numbers[i] = Random.nextInt(1, 46)
+            // 중복 검사
+            do {
+                num = Random.nextInt(1, 46)
+            } while (newNumbers.contains(num))
+            newNumbers[i] = num
+
+            Log.i("Number!!!", newNumbers[i].toString())
+        }
+
+        // 정렬
+        newNumbers.sort()
+        _numbers.value = newNumbers
+    }
+}
